15310c65952d226813536c8f2fcd9cf92ac1aa67
Sameer Ingavale <52799932+sameer-ingavale@users.noreply.github.com>
2019-11-25 19:38:57 +0530

    Added Normalization

--- plinko/score.js
+++ plinko/score.js
@@ -1,26 +1,38 @@
 const data = [];
-const testSize = 100;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   data.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
-  const [testData, trainingData] = splitData(data, testSize);
+  const testSize = 50;
+  const k = 10;
 
-  _.range(1, 16).forEach((k) => {
+  _.range(0, 3).forEach((feature) => {
+    const featureData = _.map(data, (row) => [row[feature], _.last(row)]);
+
+    const [testData, trainingData] = splitData(
+      minMax(featureData, 1),
+      testSize
+    );
+
     const accuracy = _.chain(testData)
-      .filter((dataPoint) => knn(trainingData, testData[0], k) === dataPoint[3])
+      .filter(
+        (dataPoint) =>
+          knn(trainingData, _.initial(dataPoint), k) === _.last(dataPoint)
+      )
       .size()
       .divide(testSize)
       .value();
-    console.log("Accuracy: ", k, accuracy);
+    console.log("For feature:", feature, "Accuracy: ", accuracy);
   });
 }
 
 function knn(dataSet, point, k) {
   return _.chain(dataSet)
-    .map((row) => [distance(row[0], point), row[3]])
+    .map((row) => {
+      return [distance(_.initial(row), point), _.last(row)];
+    })
     .sortBy((row) => row[0])
     .slice(0, k)
     .countBy((row) => row[1])
@@ -33,7 +45,14 @@ function knn(dataSet, point, k) {
 }
 
 function distance(pointA, pointB) {
-  return Math.abs(pointA - pointB);
+  /* console.log(pointA, pointB); */
+  return (
+    _.chain(pointA)
+      .zip(pointB)
+      .map(([a, b]) => (a - b) ** 2)
+      .sum()
+      .value() ** 0.5
+  );
 }
 
 function splitData(dataSet, testCount) {
@@ -43,4 +62,21 @@ function splitData(dataSet, testCount) {
   const trainingData = _.slice(shuffled, testCount);
 
   return [testData, trainingData];
+}
+
+function minMax(data, featureCount) {
+  const clonedData = [...data];
+
+  for (let i = 0; i < featureCount; i++) {
+    const columnData = clonedData.map((row) => row[i]);
+
+    const max = _.max(columnData);
+    const min = _.min(columnData);
+
+    for (let n = 0; n < clonedData.length; n++) {
+      clonedData[n][i] === (clonedData[n][i] - min) / (max - min);
+    }
+  }
+
+  return clonedData;
 }
